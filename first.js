6737ec5c0ee5d8ba4d5d3ada8ef3e25e43a744fa
Adeshvir Dhanoa <ad13dhanoa@gmail.com>
2022-10-22 21:23:42 -0400

    added js

--- first.js
+++ first.js
@@ -1,4 +1,4 @@
-let newInventoryCarModels = ['S1', 'T1', 'S2', 'S1', 'S2', 'S1']
+let newInventoryCarModels = ['S1', 'S1', 'S2', 'S1', 'S2', 'S1']
 let newInventoryCarTypes = ['StockCar', 'Refrigerator', 'StockCar', 'Lorrie', 'Lorrie', 'Lorrie']
 let newInventoryYearBuilt = [1922, 2011, 1975, 2002, 1965, 1951]
 
@@ -9,18 +9,27 @@ let newInventoryYearBuilt = [1922, 2011, 1975, 2002, 1965, 1951]
 //  i++
 //  }
 
- let i = 0
- let typeS1Count = 0 
+ 
+//  let i = 0
+//  let typeS1Count = 0
 
- while(i<newInventoryCarModels.length) {
-    if (newInventoryCarModels[i] == 'S1') {
-        typeS1Count+=1;
-    }
-    i++;
- }
- console.log("Number of S1 cars is:" + typeS1Count);
- 
- 
+//  while (i < newInventoryCarModels.length) {
+//      if(newInventoryCarModels[i]==='S1'){
+//         typeS1Count+=1
+//      }
+//  i++
+//  }
+
+//  console.log("Number of S1 cars is:" + typeS1Count)
+
+//  let lorrieCount = 0
+//  for (let i=0; i<newInventoryCarTypes.length ; i++) {
+//     if(newInventoryCarTypes[i]==='Lorrie'){
+//         lorrieCount+=1
+//     }
+//  }
+
+//  console.log('Number of lorries is:' + lorrieCount)
 
  
 
